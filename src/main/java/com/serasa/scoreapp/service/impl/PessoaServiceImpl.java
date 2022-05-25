6df77deec3f0d1fe2bdcabfbf0f4a2cf0dc4a26d
Letícia Lima <leticialimaferreira2@gmail.com>
2022-05-25 03:04:43 -0300

    ajuste logs

--- src/main/java/com/serasa/scoreapp/service/impl/PessoaServiceImpl.java
+++ src/main/java/com/serasa/scoreapp/service/impl/PessoaServiceImpl.java
@@ -5,6 +5,7 @@ import com.serasa.scoreapp.entity.Pessoa;
 import com.serasa.scoreapp.repository.PessoaRepository;
 import com.serasa.scoreapp.service.PessoaService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class PessoaServiceImpl implements PessoaService {
 
     private PessoaRepository pessoaRepository;
@@ -19,6 +21,7 @@ public class PessoaServiceImpl implements PessoaService {
     @Override
     public Pessoa create(Pessoa pessoa) {
         Pessoa registro = pessoa;
+        log.info("Iniciando lógica do preenchimento da descrição de score no objeto pessoa [{}]", registro);
         if(pessoa.getScore() >= 0 && pessoa.getScore() <= 1000) {
             if(registro.getScore() >= 0 && registro.getScore() <= 200) {
                 registro.setScoreDescricao("Insuficiente");
@@ -30,9 +33,11 @@ public class PessoaServiceImpl implements PessoaService {
                 registro.setScoreDescricao("Recomendável");
             }
             registro = this.pessoaRepository.save(pessoa);
+            log.info("Finalizando lógica do preenchimento da descrição de score no objeto pessoa [{}]", registro);
             return registro;
         }else{
            registro.setScoreDescricao("Score inválido. Score minímo 0 e máximo 1000");
+            log.info("Problema no input do score [{}]", registro);
            return registro;
         }
     }
